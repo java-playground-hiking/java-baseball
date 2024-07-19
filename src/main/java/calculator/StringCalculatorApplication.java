ca8f0b4a003568e4c2a06ddd0891747c86c6e3a5
ohdeng02 <sujin5792@naver.com>
2024-07-20 02:20:30 +0900

    feat: 계산결과 출력 기능

--- src/main/java/calculator/StringCalculatorApplication.java
+++ src/main/java/calculator/StringCalculatorApplication.java
@@ -4,11 +4,13 @@ import calculator.model.StringCalculator;
 import calculator.model.StringParser;
 import calculator.utils.StringExceptionHandler;
 import calculator.view.InputView;
+import calculator.view.OutputView;
 
 public class StringCalculatorApplication {
     public static void main(String[] args) {
         String[] expressionTokens = new StringParser(new InputView().getInputString()).parse();
         StringExceptionHandler stringExceptionHandler = new StringExceptionHandler(expressionTokens).validate();
         int calculationResult = new StringCalculator(stringExceptionHandler).calculate();
+        new OutputView().printCalculationResult(calculationResult);
     }
 }
